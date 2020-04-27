95a5eae00d11a8f5e136535a0e4a49ca36a87140
Abdelrahman Hamada <abdelrahman.em@gmail.com>
2020-04-27 13:33:16 +0200

    Fix special cases of pdf titles

--- bin/ren2title.py
+++ bin/ren2title.py
@@ -10,22 +10,29 @@ else:
 
 def renameFileToPDFTitle(path, fileName):
     fullName = os.path.join(path, fileName)
-    # Extract pdf title or subject from pdf file
-    newName = PdfReader(fullName).Info.Title or PdfReader(fullName).Info.Subject
-    if not newName:
-        print("No title or subject found in file: '", fileName) 
-        return
-    # Remove surrounding brackets that some pdf titles have
-    newName = newName.strip('()') + '.pdf'
-    # Rename for Windows (remove special characters)
-    newName = re.sub('[^0-9a-zA-Z\-_. ]+', ' ', newName)
-    if len(newName) < 5:
-        print("Invalid title for file: '", fileName)
-        return
-    print("Renaming file: '", fileName, "' to:", newName) 
-    newFullName = os.path.join(path, newName)
-    os.rename(fullName, newFullName)
-
+    try:
+        fileInfo = PdfReader(fullName).Info
+        if not fileInfo:
+            print("No metadata for file: '", fileName) 
+            return
+        # Extract pdf title or subject from pdf file
+        newName = fileInfo.Title or fileInfo.Subject
+        if (not newName):
+            print("No title or subject found in file: '", fileName) 
+            return
+        # Remove surrounding brackets that some pdf titles have
+        newName = newName.strip('()')
+        # Rename for Windows (remove special characters)
+        newName = re.sub('[^0-9a-zA-Z\-_. ()]+', ' ', newName)
+        if len(newName) < 1 or str(newName).lower() in ['untitled', 'none', 'null']:
+            print("Invalid title for file: '", fileName)
+            return
+        newName = newName + '.pdf'
+        print("Renaming file: '", fileName, "' to:", newName) 
+        newFullName = os.path.join(path, newName)
+        os.rename(fullName, newFullName)
+    except:
+        print("Error handling file: ", fileName) 
 
 def renPdf(directory):
     print("Renaming PDF files in: ", path)
